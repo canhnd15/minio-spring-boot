d7473637c83283dad03641e0339162e44b0b2197
canhnd <canhnd15@gmail.com>
2025-02-23 21:47:36 +0700

    generate file name

--- src/main/java/com/davidnguyen/demo/minio/service/MinIOStorageService.java
+++ src/main/java/com/davidnguyen/demo/minio/service/MinIOStorageService.java
@@ -10,6 +10,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.InputStream;
 import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 @Service
@@ -26,11 +27,13 @@ public class MinIOStorageService implements StorageService {
         this.ensureBucketExists(bucketName);
 
         for (MultipartFile file : files) {
+            String fileName = this.generateFileName(file);
+            //String fileName = UUID.randomUUID() + "/" + file.getOriginalFilename(); //Folders for versioning
             try (InputStream inputStream = file.getInputStream()) {
                 minioClient.putObject(
                         PutObjectArgs.builder()
                                 .bucket(bucketName)
-                                .object(file.getOriginalFilename())
+                                .object(fileName)
                                 .stream(inputStream, file.getSize(), -1)
                                 .contentType(file.getContentType())
                                 .build()
@@ -56,6 +59,24 @@ public class MinIOStorageService implements StorageService {
         }
     }
 
+    private String generateFileName(MultipartFile file) {
+        String originalFilename = file.getOriginalFilename();
+        if (originalFilename == null) {
+            throw new IllegalArgumentException("File name cannot be null");
+        }
+
+        String extension = "";
+        int dotIndex = originalFilename.lastIndexOf(".");
+
+        if (dotIndex != -1) {
+            extension = originalFilename.substring(dotIndex);
+            originalFilename = originalFilename.substring(0, dotIndex);
+        }
+
+        long timestamp = System.currentTimeMillis();
+        return originalFilename + "_" + timestamp + extension;
+    }
+
     @Override
     public InputStream download(String fileName) {
         try {
@@ -78,7 +99,7 @@ public class MinIOStorageService implements StorageService {
                             .bucket(bucketName)
                             .object(fileName)
                             .method(Method.GET)
-                            .expiry(30, TimeUnit.DAYS)
+                            .expiry(7, TimeUnit.DAYS) // Min: 1 days, Max 7 days
                             .build()
             );
         } catch (Exception e) {
